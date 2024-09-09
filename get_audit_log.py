6f55f52ee73b68b07aeabfaf9e145ff5bf503d16
Matteo Ferrari <matteo.ferrari@delphix.com>
2024-09-09 17:27:44 +0200

    New version with more debug capabilities and a -test_only parameter

--- get_audit_log.py
+++ get_audit_log.py
@@ -15,12 +15,12 @@
 #
 # Author  :  Marcin Przepiorowski
 # Modified:  Matteo Ferrari
-# Date    :  March 2024
+# Date    :  September 2024
 #
 # Get Delphix Continuous Compliance Engine Audit Log for a specific period
 #
 # Run example:
-# python get_audit_log.py --engine_fqdn 172.16.190.100 --username admin --password Admin-12 --start_date --end_date
+# python get_audit_log.py --engine_fqdn 172.16.190.100 --label Label1 --username admin --password Admin-12 --start_date --end_date
 #
 #        if start_date not specified, it is set to: 2000-01-01T00:00:00.000+00:00
 #        if end_date not specified,   it is set to: 2099-01-01T00:00:00.000+00:00
@@ -38,6 +38,7 @@ import json
 import sys
 # import logging
 from datetime import datetime
+
 # import time
 
 # CONFIGURATION SECTION
@@ -45,7 +46,7 @@ valid_response_status_codes = [200, 201]
 base_header = {'Content-Type': 'application/json'}
 pageSize = 500
 timeout = (30, 300)
-script_version = "Version 1.3 - 04-15-2024 con DEBUG"
+script_version = "Version 1.4 - 09-09-2024"
 
 # UNCOMMENT IT FOR DEBUG ON HTTPS LEVEL
 
@@ -66,17 +67,20 @@ def set_authorization(auth_key):
     base_header["Authorization"] = auth_key
 
 
-def post_call(url: str, request_headers: dict, request_data: dict = None, files: dict = None) -> Tuple[bool, Optional[dict]]:
+def post_call(url: str, request_headers: dict, request_data: dict = None, files: dict = None) -> Tuple[
+    bool, Optional[dict]]:
     global response
     try:
         if request_data:
             response = requests.post(url, headers=request_headers, json=request_data, timeout=timeout, verify=False)
         if files:
-            response = requests.post(url, headers=request_headers, files=files, data=request_data, timeout=timeout, verify=False)
+            response = requests.post(url, headers=request_headers, files=files, data=request_data, timeout=timeout,
+                                     verify=False)
         if not request_data and not files:
             return False, None
 
         if response.status_code not in valid_response_status_codes:
+            print("Status code: " + str(response.status_code))
             return False, None
 
         return True, response.json()
@@ -101,15 +105,15 @@ def get_call(url: str, request_headers: dict, query_params: dict = None) -> Tupl
     total = 0
     return_list = list()
 
-    while True:   
-        if query_params:            
+    while True:
+        if query_params:
             response = requests.get(url, headers=request_headers, params=query_params, timeout=timeout, verify=False)
         else:
             response = requests.get(url, headers=request_headers, timeout=timeout, verify=False)
 
         # debug
-        print(f"Status code: {response.status_code}")
-        print(f"Status code: {response.text}")
+        #print(f"Status code: {response.status_code}")
+        #print(f"Status code: {response.text}")
 
         if response.status_code not in valid_response_status_codes:
             if "is outside of the acceptable range. The last page is" in response.text:
@@ -118,7 +122,7 @@ def get_call(url: str, request_headers: dict, query_params: dict = None) -> Tupl
 
         response_obj = response.json()
         total = total + response_obj["_pageInfo"]["numberOnPage"]
-        
+
         return_list.extend(response_obj["responseList"])
         if total >= response_obj["_pageInfo"]["total"]:
             break
@@ -138,10 +142,11 @@ def get_one_call(url: str, request_headers: dict, query_params: dict = None) -> 
         response = requests.get(url, headers=request_headers, verify=False)
 
     # debug
-    print(f"Status code: {response.status_code}")
-    print(f"Status code: {response.text}")
+    #print(f"Status code: {response.status_code}")
+    #print(f"Status code: {response.text}")
 
     if response.status_code not in valid_response_status_codes:
+        print("Status code: " + str(response.status_code))
         return False, None
 
     return True, response.json()
@@ -154,9 +159,9 @@ def get_audit(base_url: str, auth_key: str) -> Tuple[bool, Optional[dict]]:
 
     set_authorization(auth_key)
 
-    api_url = f"{base_url}/audit-logs?start_time="+start_audit_date+"&end_time="+end_audit_date
+    api_url = f"{base_url}/audit-logs?start_time=" + start_audit_date + "&end_time=" + end_audit_date
 
-    print("API URL     : "+api_url)
+    print("API URL     : " + api_url)
 
     success, response = get_call(api_url, base_header)
     if not success:
@@ -169,7 +174,7 @@ def login(base_url: str, user: str, password: str) -> Tuple[bool, Optional[dict]
     #
     # login to engine
     #
-    
+
     api_url = f"{base_url}/login"
     request_data = dict()
     request_data['username'] = user
@@ -189,11 +194,13 @@ if __name__ == "__main__":
     parser.add_argument('--engine_fqdn', type=str, help='engine fqdn')
     parser.add_argument('--username', type=str, help='username')
     parser.add_argument('--password', type=str, help='password')
+    parser.add_argument('--label', type=str, help='label to help filtering the log records')
     parser.add_argument('--start_date', type=str, help='start date for audit log')
     parser.add_argument('--end_date', type=str, help='end date for audit log')
     parser.add_argument('--engine_offset', type=str, help='engine offset (in the form "+00:00" or "-00:00"')
     parser.add_argument('--output', type=str, help='file name that will contain the log records')
     parser.add_argument('--version', action='store_true', help='prints version of this program')
+    parser.add_argument('--test_only', action='store_true', help='test only execution. does not5 save last run info.')
 
     args = parser.parse_args()
 
@@ -201,10 +208,14 @@ if __name__ == "__main__":
     print(" Delphix Continuous Compliance Audit Log extraction")
     print("=====================================================================")
     # set default start and end date if not specified
-      
+
     if args.version:
         print(script_version)
         sys.exit(0)
+
+    if not args.label:
+        print("Error: label must be specified.")
+        sys.exit(8)
 
     if not args.engine_fqdn:
         print("Error: engine name or IP must be specified.")
@@ -226,47 +237,52 @@ if __name__ == "__main__":
         args.engine_offset = "+00:00"
 
     if not args.start_date:
-        args.start_date = "2000-01-01T00:00:00.000"+args.engine_offset
+        args.start_date = "2000-01-01T00:00:00.000" + args.engine_offset
     else:
-        args.start_date = args.start_date+args.engine_offset
-
+        args.start_date = args.start_date + args.engine_offset
 
     if not args.end_date:
-        args.end_date = datetime.now().strftime('%Y-%m-%dT%H:%M:%S.%f')[:-3]+args.engine_offset
+        args.end_date = datetime.now().strftime('%Y-%m-%dT%H:%M:%S.%f')[:-3] + args.engine_offset
     else:
-        args.end_date = args.end_date+args.engine_offset
+        args.end_date = args.end_date + args.engine_offset
 
     # args.end_date = "2099-01-01T00:00:00.000"+args.engine_offset
 
     engine_fqdn = args.engine_fqdn
+    label = args.label
     username = args.username
     password = args.password
     auth_key = ""
     last_exec_file = ".last_audit_extract_" + engine_fqdn
 
     # read last execution from file
-
-    if os.path.isfile(last_exec_file):
-       with open(last_exec_file, 'r') as file:
-            args.start_date = file.read()
+    if not args.test_only:
+        if os.path.isfile(last_exec_file):
+            with open(last_exec_file, 'r') as file:
+                args.start_date = file.read()
 
     print("Engine      : " + args.engine_fqdn)
+    print("Label       : " + args.label)
     print("Start       : " + args.start_date)
     print("End         : " + args.end_date)
     print("Output file : " + args.output)
-    
+    if args.test_only:
+        print("***  TEST-ONLY Execution  ***")
+
     start_audit_date = requests.utils.quote(args.start_date)
     end_audit_date = requests.utils.quote(args.end_date)
 
+    #
     # Execution
+    #
 
     base_url = f"https://{engine_fqdn}/masking/api"
-    
+
     ret_status, response = login(base_url, username, password)
     if ret_status:
-        auth_key = response["Authorization"] 
+        auth_key = response["Authorization"]
     else:
-        print("Problem during login.")
+        print("Problem during login. Check engine type, user credentials and user role (must be admin)." )
         sys.exit(8)
 
     ret_status, response = get_audit(base_url, auth_key)
@@ -274,17 +290,19 @@ if __name__ == "__main__":
         f = open(args.output, "w")
         countr = 0
         for audit_item in sorted(response, key=lambda audit_entry: audit_entry["auditId"]):
-
             # enrich json with engine name
-            final_audit_item = {**{"engine": engine_fqdn}, **audit_item}
-            countr = countr +1
-            f.write(json.dumps(final_audit_item)+'\n')
+            audit_item2 = {**{"label": label}, **audit_item}
+            final_audit_item = {**{"engine": engine_fqdn}, **audit_item2}
+            countr = countr + 1
+            f.write(json.dumps(final_audit_item) + '\n')
         f.close()
         print("Records out : " + str(countr))
+
         # write last execution
-        with open(last_exec_file, "w") as f:
-            f.write(args.end_date)
+        if not args.test_only:
+            with open(last_exec_file, "w") as f:
+                f.write(args.end_date)
 
     else:
-        print("Problem with getting audit entries")
+        print("Problem with getting audit entries.")
         sys.exit(8)
